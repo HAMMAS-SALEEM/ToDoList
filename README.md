5a46aeec51fe83fb7d5a4addb14cf921d6e04fa3
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-12 21:38:24 +0500

    Add list Structure

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ToDoList
+# webpack
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },{
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      use: [
+        'url-loader?limit=10000',
+        'img-loader'
+      ]
+    },{
+      test: /\.html$/,
+      use: ['html-loader']
+    }],
+  },
+};
