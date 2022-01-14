8fe500d9b4fb55d4e3c6d0cf213f9c15b3c4544b
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-14 12:14:38 +0500

    Update script, markup, style for  project

--- src/CRUD.js
+++ src/CRUD.js
@@ -1,22 +1,65 @@
-export const addItem = (arr,value) => {
-    if(JSON.parse(localStorage.getItem('todos'))==null) {
+export const addItem = (arr, value) => {
+    if (JSON.parse(localStorage.getItem('todos')) == null) {
         arr = []
-    }
-        else {
+    } else {
         arr = JSON.parse(localStorage.getItem('todos'))
     }
 
     let length = arr.length
     console.log(arr)
-    let obj = {description: value, completed: false, index: length+=1}
+    let obj = {
+        description: value,
+        completed: false,
+        index:length
+    }
     arr.push(obj)
-    localStorage.setItem('todos',JSON.stringify(arr))
+    localStorage.setItem('todos', JSON.stringify(arr))
 }
 
 export const removeItem = (id) => {
     console.log(id)
     let arr = JSON.parse(localStorage.getItem('todos'))
-    arr = arr.filter(e=>e.index != id)
+    arr = arr.filter(e => e.index != id)
     localStorage.setItem('todos', JSON.stringify(arr))
 }
 
+export const updateItem = (arr,id,name)=>{
+    console.log(arr.length)
+    for(let i=0;i<arr.length;i+=1){
+      if(arr[i].id==id) {
+        arr[i].description=name
+      }
+    }
+ }
+
+ export const displayToDos = (locStorage, output) => {
+    output.innerHTML = null
+    locStorage.forEach((item) => {
+        output.innerHTML += `<li class="todos">
+          <ul class="todos-01" >
+          <li><input type="checkbox" class=" toDoItems"></li>
+          <li><input type='text' value="${item.description}" class="toDoItems tdt" id=${item.index} readOnly></input></li>
+          </ul>
+          <i class="fas fa-ellipsis-v"></i>
+          <i class="fas fa-trash-alt" id="${item.index}"></i>
+      </li>`;
+    })
+};
+
+
+// export const display = () => {
+//     let output = document.createElement('ul')
+//     output.classList.add('todos')
+//     let li = document.createElement('li');
+//     li.classList.add('todos-01')
+//     output.appendChild(li)
+//     let ul2 = document.createElement('ul')
+//     ul2.classList.add('toDoItems')
+//     li.appendChild(ul2)
+//     let li2 = document.createElement('li')
+//     ul2.appendChild(li2)
+//     let input01 = document.createElement('input')
+//     input01.classList.add('toDoItems')
+//     li2.appendChild(input01)
+//     console.log(output)
+// }
--- src/index.js
+++ src/index.js
@@ -1,49 +1,62 @@
 import './style.css';
-import {addItem,removeItem} from './CRUD.js'
+import {
+  addItem,
+  removeItem,
+  updateItem,
+  displayToDos
+} from './CRUD.js'
+import {
+  update
+} from 'lodash';
 
 const todoContainer = document.querySelector('.todos-container');
 const addBtn = document.querySelector('.fa-plus')
+const trashBtn = document.querySelector('.fa-trash-alt')
+const dragBtn = document.querySelector('.fa-ellipsis-v')
 const input = document.querySelector('.toDoName')
-const removeBtn = document.getElementsByClassName('fa-trash-alt')
+const locStorage = JSON.parse(localStorage.getItem('todos'));
+let updateArr;
 
-let arr = [];
+let arr;
+if (locStorage == null) {
+  arr = []
+} else {
+  arr = locStorage
+}
 
-const displayToDos = () => {
-  arr = JSON.parse(localStorage.getItem('todos'))
-  arr.forEach((item) => {
-    todoContainer.innerHTML += `<li class="todos widthHeight">
-        <ul class="todos-01">
-        <li><input type="checkbox" class=" toDoItems"></li>
-        <li><p class="toDoItems tdt" contenteditable="true">${item.description}</p></li>
-        </ul>
-        <i class="fas fa-ellipsis-v"></i>
-        <i class="fas fa-trash-alt" id="${item.index}"></i>
-    </li>`;
-  })
-};
 
-addBtn.addEventListener('click', ()=>{
+
+addBtn.addEventListener('click', () => {
+  const todoContainer = document.querySelector('.todos-container');
   let inputVal = input.value
-  addItem(arr,inputVal)
-  displayToDos();
+  addItem(arr, inputVal)
+  const locStorage = JSON.parse(localStorage.getItem('todos'));
+  displayToDos(locStorage, todoContainer);
 })
 
 
-todoContainer.addEventListener('click' ,(e)=>{
-  if(e.target.tagName=='I'){
+todoContainer.addEventListener('click', (e) => {
+  if (e.target.tagName == 'I' && e.target.classList.value == 'fas fa-trash-alt') {
     let id = e.target.id
-    console.log(id)
-    removeItem(id)
-  }  
+    removeItem(id);
+    let locStorage = JSON.parse(localStorage.getItem('todos'));
+    displayToDos(locStorage, todoContainer);
+    input.value = ''
+  }
 })
 
-// todoContainer.addEventListener('click',(e)=>{
-//   let parent= e.target.parentNode
-//   console.log(parent.children)
-//   // if(e.target.tagName == 'I'){
-//   //   let id = e.target.id;
-//   //   console.log(id)
-//   // }
-// })
+todoContainer.addEventListener('click', (e) => {
+  if (e.target.tagName == 'INPUT') {
+    let value = e.target.value
+    console.log(value)
+    let arr = JSON.parse(localStorage.getItem('todos'));
+    e.target.readOnly = false;
+    let id = e.target.id
+    updateItem(arr, id, value)
+    localStorage.setItem('todos', JSON.stringify(arr))
+  }
+})
 
-displayToDos();
+window.addEventListener('load', () => {
+  displayToDos(arr, todoContainer)
+})
