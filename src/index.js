7175666da0a1414ec1ff693e9138a4a950b2fd57
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-19 15:52:08 +0500

    Fix linters

--- src/index.js
+++ src/index.js
@@ -26,12 +26,12 @@ window.addEventListener('load', () => {
 });
 
 addBtn.addEventListener('click', () => {
-    const todoContainer = document.querySelector('.todos-container');
-    const inputVal = input.value;
-    addItem(arr, inputVal);
-    const locStorage = JSON.parse(localStorage.getItem('todos'));
-    input.value = '';
-    displayToDos(locStorage, todoContainer);
+  const todoContainer = document.querySelector('.todos-container');
+  const inputVal = input.value;
+  addItem(arr, inputVal);
+  const locStorage = JSON.parse(localStorage.getItem('todos'));
+  input.value = '';
+  displayToDos(locStorage, todoContainer);
 });
 
 todoContainer.addEventListener('click', (e) => {
